8deb445bb3216221f853c5f774c25c142a230455
haminji <h.river771@gmail.com>
2020-01-10 15:32:06 +0900

    combo button

--- ToyWriter_ver1/MainWindow.xaml.cs
+++ ToyWriter_ver1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
 
 namespace ToyWriter_ver1
 {
@@ -23,6 +25,42 @@ namespace ToyWriter_ver1
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<int> years = new List<int>();
+            for (int i=DateTime.Today.Year; i>=1950; i--)
+            {
+                years.Add(i);
+            }
+            comboYear.ItemsSource = years;
+            //listPerson.ItemsSource = listData;
+            //ComboBox comboYear = new ComboBox();
+            //for (int i = 1950; i <= DateTime.Today.Year; i++)
+            //{
+            //    ComboBoxItem item = new ComboBoxItem();
+            //    item.Content = i;
+            //    comboYear.Items.Add(item);
+            //}
+        }
+        private void btnPath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void comboYear_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
